89059e5d77ad601b4e760ac396c3ed6e19cc8389
Arjun <thagullaa1@gmail.com>
2021-02-05 05:16:36 +0530

    adding file

---
+++ Free.java
@@ -0,0 +1,6 @@
+class Free{
+
+	public static void main(String [] args){
+
+}
+}
--- Test.java
+++ Test.java
@@ -3,5 +3,11 @@ class Test{
 	public static void main(String[] args){
 
 	int a =10;
+
+	public void m1(){
+
+		
+
+}
     }
 }
